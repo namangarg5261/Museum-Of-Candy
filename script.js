f8ae04a6cc9d3b1923a3f0fca3437c7d9e89787d
namangarg5261 <namangarg5261@gmail.com>
2020-06-16 22:02:36 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var userName = prompt("what is your first name")
+var lastName = prompt("what isyour last name")
+var age = prompt("how old are you")
+console.log ("nice to meet you" + userName + lastName)
+console.log("you are" + age + "years old")
